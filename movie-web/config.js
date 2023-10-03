573a067de0edc6dd5456a79b9cfc2c0ec66dd443
timliniscool <146823314+timliniscool@users.noreply.github.com>
2023-10-03 20:12:53 +1100

    heheheha

---
+++ movie-web/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://timovies.timctro-dscgd.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTUyYzM4MzAwMTRjYmEwZTkyZDE3NzA2NzEyZGY0YSIsInN1YiI6IjY1MWJkNWU0MDcyMTY2MDBhY2I1YTVkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p91D2N38TOjjR49p0_wgKnW88BICL15IxmZQwwb7eyA"
+};
